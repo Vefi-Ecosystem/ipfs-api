315a0cd228736ae0ad367970f83eb686a979c7a1
Kingsley Victor <javaprodigy56@gmail.com>
2022-05-08 22:02:26 +0100

    initialize repo

---
+++ src/main/java/org/vefi/ipfs/VefiIpfsApiApplication.java
@@ -0,0 +1,13 @@
+package org.vefi.ipfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VefiIpfsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VefiIpfsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vefi/ipfs/VefiIpfsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vefi.ipfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VefiIpfsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
